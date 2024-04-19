3905e614bafe70c95f0f63a003258201d69e4a73
John Newall <john@johnnewall.com>
2024-04-19 22:07:11 +0930

    revised templates and content

---
+++ src/content/block_01.md
@@ -0,0 +1,6 @@
+title:   Lorem Ipsum 01
+authors: j0ono0
+created:    2024-4-18
+updated:   2024-4-18
+
+Block 01 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisi quis eleifend quam adipiscing vitae proin sagittis. Varius vel pharetra vel turpis nunc eget lorem dolor sed. Adipiscing bibendum est ultricies integer quis. Congue quisque egestas diam in arcu cursus euismod quis viverra.
--- src/site_generator.py
+++ src/site_generator.py
@@ -8,6 +8,7 @@ from jinja2 import (
 import markdown
 from utilities import copy_dir
 
+
 BUILD_PATH = Path("../")
 COMPONENT_PATH = Path("./components")
 TEMPLATE_PATH = Path("./templates")
@@ -43,27 +44,43 @@ for dir in content_list:
         copy_dir(src, dst)
 
 
-def parse_markdown(template, src):
+def parse_md_file(path):
     md = markdown.Markdown(extensions=["meta"])
-    ctx = {"src": src}
-    with open(src) as f:
-        ctx["content"] = md.convert(f.read())
+    with open(path) as f:
+        html = md.convert(f.read())
+        meta = {key: ", ".join(val) for (key, val) in md.Meta.items()}
+    return (html, meta)
 
-    for key in md.Meta:
-        ctx[key] = ", ".join(md.Meta[key])
 
-    return template.render(ctx)
+#### build some common components ######################################
+analytics_html = templates.get_template("analytics.jinja").render()
+
+navdata = {
+    "class": "sitenav",
+    "items": [
+        {"text": "home", "slug": "index.html"},
+        {"text": "blocks", "slug": "3-section-blocks.html"},
+    ],
+}
+
+sitenav_tplt = templates.get_template("nav.jinja")
 
 
 ### build page #########################
 
 # page template
 page_tplt = templates.get_template("minima/minima.jinja")
+context = {
+    "slug": "index.html",
+    "doc_title": "Genie-rator",
+    "head": [analytics_html],
+    "body": [],
+    "navdata": navdata,
+}
+
 
 # extra content for page head (some in minima template already!)
-head = []
-head.append(templates.get_template("analytics.jinja").render())
-head.append(
+context["head"].append(
     templates.get_template("html_meta.jinja").render(
         meta={
             "author": "j0ono0",
@@ -73,20 +90,47 @@ head.append(
 )
 
 # content for page body
-body = []
 
 section_tplt = templates.get_template("minima/section.jinja")
 for mdfile in ["intro.md", "ideas.md"]:
-    src_path = Path("content", mdfile)
-    body.append(parse_markdown(section_tplt, src_path))
+    md_html, md_meta = parse_md_file(Path("content", mdfile))
+    context["body"].append(section_tplt.render(content=md_html, **md_meta))
 
-# add collated content to template
-page_ctx = {"doc_title": "Genie-rator", "head": head, "body": body}
-
+# Render template with context
 with open(BUILD_PATH / "index.html", "w") as f:
-    f.write(page_tplt.render(page_ctx))
+    f.write(page_tplt.render(context))
+
 
 ########################################################################
 
+# Build another page, see if some patterns appear?
+page_tplt = templates.get_template("minima/minima.jinja")
+context = {
+    "slug": "3-section-blocks.html",
+    "doc_title": "3 content blocks",
+    "head": [analytics_html],
+    "body": [],
+    "navdata": navdata,
+}
+
+# Add head content
+context["head"].append(
+    templates.get_template("html_meta.jinja").render(
+        meta={
+            "author": "j0ono0",
+            "description": "3 content blocks assembled with a static site generator.",
+        }
+    )
+)
+
+# Add body content
+for mdfile in ["block_01.md", "block_02.md", "block_03.md"]:
+    html, meta = parse_md_file(Path("content", mdfile))
+    context = {**context, **meta}
+    context["body"].append(html)
+
+# render content to final layout and save as html file
+with open(BUILD_PATH / "3-section-blocks.html", "w") as f:
+    f.write(page_tplt.render(context))
 
 print("\nGenie generation complete\n")
