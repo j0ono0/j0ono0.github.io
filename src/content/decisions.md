ba5515193a74d8ee02d87ac5e3d8994e2f645784
John Newall <john@johnnewall.com>
2024-04-20 11:57:51 +0930

    use macros in templates

---
+++ src/content/decisions.md
@@ -0,0 +1,14 @@
+title:   Decisions
+authors: j0ono0
+created:    2024-4-20
+updated:   2024-4-20
+
+## Some decisions!
+
+Not final decisions or conclusions necessarly, but thoughts that are currently guiding my ideas and choices.
+
+- Markdown is nice, but I could also support .rst?
+- Content in markdown should be strictly about the document.
+  Meta data in the markdown file enhances the file BUT this content should strictly relate to the markdown content and not the website. eg including a 'url' or 'taxonomy tags' in the md file is a bad idea.
+- Macros are a nice way to control template components. The allow clean code for documenting page content.
+- Separating the generator code and site content code into x2 repos could be helpful for project organisation and possible distribution later
--- src/site_generator.py
+++ src/site_generator.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import os
 from jinja2 import (
     Environment,
     FileSystemLoader,
@@ -8,24 +7,21 @@ from jinja2 import (
 import markdown
 from utilities import copy_dir
 
-
 BUILD_PATH = Path("../")
-COMPONENT_PATH = Path("./components")
 TEMPLATE_PATH = Path("./templates")
 CONTENT_PATH = Path("./content")
 
 # Ensure paths exist
 BUILD_PATH.mkdir(parents=True, exist_ok=True)
 
-
-tplt_dirs = ["templates", "components"]
+# Jinja2 environment setup
+tplt_dirs = [TEMPLATE_PATH]
 templates = Environment(
     loader=FileSystemLoader(tplt_dirs),
     autoescape=select_autoescape(),
     trim_blocks=True,
     lstrip_blocks=True,
 )
-
 
 #### copy assets from template themes to build dir #######################################
 theme_list = [d for d in TEMPLATE_PATH.iterdir() if d.is_dir()]
@@ -52,85 +48,68 @@ def parse_md_file(path):
     return (html, meta)
 
 
-#### build some common components ######################################
-analytics_html = templates.get_template("analytics.jinja").render()
+#### Common components ######################################
 
-navdata = {
-    "class": "sitenav",
-    "items": [
-        {"text": "home", "slug": "index.html"},
-        {"text": "blocks", "slug": "3-section-blocks.html"},
-    ],
-}
+site_nav = [
+    {"text": "home", "slug": "index.html"},
+    {"text": "blocks", "slug": "3-section-blocks.html"},
+    {"text": "ideas", "slug": "ideas.html"},
+    {"text": "decisions", "slug": "decisions.html"},
+]
 
-sitenav_tplt = templates.get_template("nav.jinja")
+### assemble pages #########################
 
+pages = [
+    {
+        "template": templates.get_template("minima/minima.jinja"),
+        "slug": "index.html",
+        "doc_title": "Genie-rator",
+        "nav": site_nav,
+        "head": [
+            ("metatag", ("author", "j0ono0")),
+            ("metatag", ("description", "A genie that generates static sites")),
+            ("google_analytics", ()),
+        ],
+        "main": [
+            ("md_section", (parse_md_file(Path("content", "intro.md")))),
+            ("md_section", (parse_md_file(Path("content", "ideas.md")))),
+        ],
+        "footer": [],
+    },
+    {
+        "template": templates.get_template("minima/minima.jinja"),
+        "slug": "3-section-blocks.html",
+        "doc_title": "Content collated into in blocks",
+        "nav": site_nav,
+        "head": [
+            ("metatag", ("author", "j0ono0")),
+            ("metatag", ("description", "3 content blocks assembled together.")),
+            ("google_analytics", ()),
+        ],
+        "main": [
+            ("md_section", (parse_md_file(Path("content", "block_01.md")))),
+            ("md_section", (parse_md_file(Path("content", "block_02.md")))),
+            ("md_section", (parse_md_file(Path("content", "block_03.md")))),
+        ],
+    },
+    {
+        "template": templates.get_template("minima/minima.jinja"),
+        "slug": "decisions.html",
+        "doc_title": "Decisions for Genie-rator",
+        "nav": site_nav,
+        "head": [
+            ("metatag", ("author", "j0ono0")),
+            ("metatag", ("description", "Ideas for Python genie-rator.")),
+            ("google_analytics", ()),
+        ],
+        "main": [
+            ("md_section", (parse_md_file(Path("content", "decisions.md")))),
+        ],
+    },
+]
 
-### build page #########################
-
-# page template
-page_tplt = templates.get_template("minima/minima.jinja")
-context = {
-    "slug": "index.html",
-    "doc_title": "Genie-rator",
-    "head": [analytics_html],
-    "body": [],
-    "navdata": navdata,
-}
-
-
-# extra content for page head (some in minima template already!)
-context["head"].append(
-    templates.get_template("html_meta.jinja").render(
-        meta={
-            "author": "j0ono0",
-            "description": "A static site generator experiment using Python.",
-        }
-    )
-)
-
-# content for page body
-
-section_tplt = templates.get_template("minima/section.jinja")
-for mdfile in ["intro.md", "ideas.md"]:
-    md_html, md_meta = parse_md_file(Path("content", mdfile))
-    context["body"].append(section_tplt.render(content=md_html, **md_meta))
-
-# Render template with context
-with open(BUILD_PATH / "index.html", "w") as f:
-    f.write(page_tplt.render(context))
-
-
-########################################################################
-
-# Build another page, see if some patterns appear?
-page_tplt = templates.get_template("minima/minima.jinja")
-context = {
-    "slug": "3-section-blocks.html",
-    "doc_title": "3 content blocks",
-    "head": [analytics_html],
-    "body": [],
-    "navdata": navdata,
-}
-
-# Add head content
-context["head"].append(
-    templates.get_template("html_meta.jinja").render(
-        meta={
-            "author": "j0ono0",
-            "description": "3 content blocks assembled with a static site generator.",
-        }
-    )
-)
-
-# Add body content
-for mdfile in ["block_01.md", "block_02.md", "block_03.md"]:
-    html, meta = parse_md_file(Path("content", mdfile))
-    context = {**context, **meta}
-    context["body"].append(html)
-
-# render content to final layout and save as html file
-with open(BUILD_PATH / "3-section-blocks.html", "w") as f:
-    f.write(page_tplt.render(context))
+for page in pages:
+    with open(BUILD_PATH / page["slug"], "w") as f:
+        f.write(page["template"].render(page))
 
 print("\nGenie generation complete\n")
